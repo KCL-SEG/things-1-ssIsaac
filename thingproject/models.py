d4dedaffad2ebae3de57597bc00d3cb2f78ce00f
Isaac Leegit config --global user.name Isaac <shun.s.lee@kcl.ac.uk>
2022-12-09 15:11:51 +0000

    exe3

--- thingproject/models.py
+++ thingproject/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
     
-    name = models.CharField(max_length = 30, unique = True)
-    description = models.CharField(max_length = 100, blank = False)
-    quanity = models.IntegerField(blank = False)
+    name = models.CharField(max_length = 30, unique = True, blank=False)
+    description = models.CharField(max_length = 120, blank = True, unique=False)
+    quanity = models.IntegerField(
+        unique=False, 
+        validators=[MaxValueValidator(100),
+            MinValueValidator(1) 
+            ]
+        )
     
     
